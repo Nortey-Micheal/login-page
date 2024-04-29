bf7a7004b4c45de52285b45e3c930a3f8803aa4b
Nortey-Micheal <norteymicheal91@gmail.com>
2024-04-29 12:09:07 -0700

    "Added interactivity to sign up and log in tabs, updated form field validations, and made various cosmetic changes to the index page"
    Here's a breakdown of the changes made in the code diff:
    
    * Add

---
+++ index.js
@@ -0,0 +1,47 @@
+const  div1 = document.querySelector("main>div>div:first-of-type");
+const divs = document.querySelectorAll("main>div>div");
+
+window.addEventListener("load", () =>{
+    div1.style.backgroundColor = "#030347";
+    div1.style.color = "#E6E6FA";
+    div1.classList.add("activated");
+})
+
+const check = (event) =>{
+    if (event.target == divs[0] || event.target == divs[1]){
+        parent = event.target;
+    } else if (event.target.parentNode == divs[0] || event.target.parentNode == divs[1]){
+        parent = event.target.parentNode;
+    } else if(event.target.parentNode.parentNode == divs[0] || event.target.parentNode.parentNode == divs[1]){
+        parent = event.target.parentNode.parentNode;
+    } else if(event.target.parentNode.parentNode.parentNode == divs[0] || event.target.parentNode.parentNode.parentNode == divs[1]){
+        parent = event.target.parentNode.parentNode.parentNode;
+    } else if (event.target.parentNode.parentNode.parentNode.parentNode == divs[0] || event.target.parentNode.parentNode.parentNode.parentNode == divs[1]){
+        parent = event.target.parentNode.parentNode.parentNode.parentNode;
+    } else if (event.target.parentNode.parentNode.parentNode.parentNode.parentNode == divs[0] || event.target.parentNode.parentNode.parentNode.parentNode.parentNode == divs[1]){
+        parent = event.target.parentNode.parentNode.parentNode.parentNode.parentNode;
+    }
+}
+
+const changeCol = (event) => {
+    check(event);
+    if(parent == divs[0]){
+        divs[0].style.backgroundColor = "#030347";
+        divs[0].style.color = "#E6E6FA";
+        divs[0].classList.add("activated");
+        divs[1].style.backgroundColor = "#E6E6FA";
+        divs[1].style.color = "#030347";
+        divs[1].classList.remove("activated");
+    } else if (parent == divs[1]){
+        divs[1].style.backgroundColor = "#030347";
+        divs[1].style.color = "#E6E6FA";
+        divs[1].classList.add("activated");
+        divs[0].style.backgroundColor = "#E6E6FA";
+        divs[0].style.color = "#030347";
+        divs[0].classList.remove("activated");
+    } else if(parent == window) {
+        console.log("Fuck")
+    }
+}
+divs.forEach(addEventListener("click", changeCol))
+
