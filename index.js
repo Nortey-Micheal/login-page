13c753a099d61860f6eb47929de24a7317369512
Nortey-Micheal <norteymicheal91@gmail.com>
2024-04-30 02:30:25 -0700

    Update CSS and JS for sign up and log in forms; add not-active class to hide inactive form
    The CSS has been updated to style the footer and header, and to add a not-active class that hides certain elements

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const  div1 = document.querySelector("main>div>div:first-of-type");
 const divs = document.querySelectorAll("main>div>div");
+const links = document.querySelectorAll("main>div>div p>a");
 
 window.addEventListener("load", () =>{
     div1.style.backgroundColor = "#030347";
     div1.style.color = "#E6E6FA";
     div1.classList.add("activated");
+    divs[1].classList.add("not-active");
 })
 
 const check = (event) =>{
@@ -23,25 +25,43 @@ const check = (event) =>{
     }
 }
 
+
+// const verifyScrn = (event) => {
+//     // handleclick();
+//     if (event.target.innerText == "Log in"){
+//         divs[1].classList.remove("not-active");
+//         divs[0].classList.add("not-active");
+//     } else if(event.target.innerText == "Sign up"){
+//         divs[0].classList.remove("not-active");
+//         divs[1].classList.add("not-active");
+//     }
+// }
+
 const changeCol = (event) => {
     check(event);
     if(parent == divs[0]){
         divs[0].style.backgroundColor = "#030347";
         divs[0].style.color = "#E6E6FA";
         divs[0].classList.add("activated");
+        divs[0].classList.remove("not-active");
         divs[1].style.backgroundColor = "#E6E6FA";
         divs[1].style.color = "#030347";
         divs[1].classList.remove("activated");
+        divs[1].classList.add("not-active");
     } else if (parent == divs[1]){
         divs[1].style.backgroundColor = "#030347";
         divs[1].style.color = "#E6E6FA";
         divs[1].classList.add("activated");
+        divs[1].classList.remove("not-active");
         divs[0].style.backgroundColor = "#E6E6FA";
         divs[0].style.color = "#030347";
         divs[0].classList.remove("activated");
+        divs[0].classList.add("not-active");
     } else if(parent == window) {
         console.log("Fuck")
     }
 }
-divs.forEach(addEventListener("click", changeCol))
+divs.forEach(addEventListener("click", changeCol));
+
+// links.forEach(addEventListener("click" , verifyScrn));
 
